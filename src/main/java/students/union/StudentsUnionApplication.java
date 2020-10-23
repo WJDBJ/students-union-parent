24774b1518765327543b6913e685af79a851ee87
jianxiao <1286270834@qq.com>
2020-10-23 11:21:48 +0800

    Initial commit

---
+++ src/main/java/students/union/StudentsUnionApplication.java
@@ -0,0 +1,15 @@
+package students.union;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author: jianxiao
+ * @date: 2020/10/23 11:16
+ */
+@SpringBootApplication
+public class StudentsUnionApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(StudentsUnionApplication.class,args);
+    }
+}
